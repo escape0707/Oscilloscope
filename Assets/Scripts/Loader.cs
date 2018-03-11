63ee26c07145d6f5f3e10fb1648444ebe8a81b1f
朱志海 <tothesong@gmail.com>
2018-03-11 17:44:45 +0800

    Add GameManager singleton and Loader

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+    public GameObject gameManager;
+    void Awake() {
+        Instantiate(gameManager);
+    }
+}
